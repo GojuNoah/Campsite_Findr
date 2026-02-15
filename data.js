592b2da14e392e05f0f1ad90b50ba6a70c56d0a2
Noah Stone <gojunoah@gmail.com>
2026-02-15 13:28:08 -0600

    Add national forests data to data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const nationalForests = [
+  { 
+    name: "Ouachita National Forest", 
+    latitude: 34.500416, 
+    longitude: -94.500687, 
+    link: "https://www.fs.usda.gov/r08/ouachita",
+    dispersedCampingAllowed: true,
+    imageURL: "https://www.fs.usda.gov/sites/nfs/files/styles/hero/public/legacy-media/ouachita/Hero%20Image%202%20Flatside%20Wilderness.jpg?h=f7d9296c&itok=MiVkb8CU", 
+    description: "Located in both Arkansas and Oklahoma, the Ouachita National Forest is home to rolling hills, pristine lakes and geological wonders.";
+  },
+  {
+    name: "Allegheny National Forest",
+    latitude: 41.6488,
+    longitude: 79.0061,
+    link: "https://www.fs.usda.gov/r09/allegheny",
+    dispersedCampingAllowed: true,
+    imageURL: "https://www.fs.usda.gov/sites/nfs/files/styles/hero/public/r09/allegheny/image/20250103_BeaverMeadows_0.jpg?h=19f14c2c&itok=HNds1VeZ",
+    description: "Established in 1923, the Allegheny National Forest is comprised of over half a million acres in the northwest corner of Pennsylvania.";
+  },
+  {
+    name: "Angeles National Forest",
+    latitude: 34.4113,
+    longitude: 118.1751,
+    link: "https://www.fs.usda.gov/r05/angeles",
+    dispersedCampingAllowed: true,
+    imageURL: "https://www.fs.usda.gov/sites/nfs/files/styles/hero/public/legacy-media/angeles/Angeles%20NF%20Hero%20Image%201000%20pixels.jpg?h=9624e4e4&itok=0V-KKtiI",
+    description: "From the high desert to mountains, the forest is the backyard playground to 20+ M people in the Greater Los Angeles Metro Area.";
+  },
+]
