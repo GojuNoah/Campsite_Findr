ec5ac80d8117e3e7a84837839323cab734a56ede
Noah Stone <gojunoah@gmail.com>
2026-02-19 18:17:31 -0600

    Add multiple national forests data entries

--- data.js
+++ data.js
@@ -26,4 +26,31 @@ const nationalForests = [
     imageURL: "https://www.fs.usda.gov/sites/nfs/files/styles/hero/public/legacy-media/angeles/Angeles%20NF%20Hero%20Image%201000%20pixels.jpg?h=9624e4e4&itok=0V-KKtiI",
     description: "From the high desert to mountains, the forest is the backyard playground to 20+ M people in the Greater Los Angeles Metro Area."
   },
+  {
+    name: "Angelina National Forest",
+    latitude: 31.4949,
+    longitude: 94.7749,
+    link: "https://www.fs.usda.gov/r08/texas/recreation/angelina-national-forest",
+    dispersedCampingAllowed: true,
+    imageURL: "https://upload.wikimedia.org/wikipedia/commons/5/5f/Angelina_National_Forest_sign.jpg",
+    description: "Located in the heart of east Texas, the 153,179-acre Angelina National Forest is located in Angelina, Nacogdoches, San Augustine and Jasper counties."
+  },
+  {
+    name: "Apache-Sitgreaves National Forests",
+    latitude: 34.1408,
+    longitude: 109.2865,
+    link: "https://www.fs.usda.gov/r03/apache-sitgreaves",
+    dispersedCampingAllowed: true,
+    imageURL: "https://www.fs.usda.gov/sites/nfs/files/styles/hero/public/r03/apache-sitgreaves/image/Rim%20Overlook%20Winter%20SCox.JPG?h=c0c2b5e3&itok=O6pZjPVW",
+    description: "The ASNF is home to a large part of the Mogollon Rim and White Mountains, rich in beauty and tranquility. Water is a major attraction with 24 lakes and reservoirs and more than 400 miles of rivers and streams—more than can be found in any other southwestern forest."
+  },
+  {
+    name: "Apalachicola National Forest",
+    latitude: 30.1931051,
+    longitude: 84.6939571,
+    link: "https://www.fs.usda.gov/r08/florida/recreation/apalachicola-national-forest",
+    dispersedCampingAllowed: true,
+    imageURL: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Apalachicola_pond.JPG/330px-Apalachicola_pond.JPG",
+    description: "The Apalachicola National Forest is home to some of the most unique animal and plant species in the world. Here, visitors can enjoy safe, family-friendly activities such as fishing, hunting, hiking and trail-riding while surrounded by tranquil, diverse ecosystems."
+  },
 ]
