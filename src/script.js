b034080c0d9a39d03c8d420dd0f9b4cd3f59abef
Noah Stone <noahstone@Noahs-MacBook-Air.local>
2026-06-01 21:10:41 -0500

    added navbar and subtitle to hero

--- src/script.js
+++ src/script.js
@@ -61,7 +61,6 @@ form.addEventListener('submit', async function(event) {
       <div class="forestCard">
 			  <img class="forestImage" src="${nationalForests.imageURL}" alt="Forest Image">
 			  <h2 class="forestName">${nationalForests.name}</h2>
-			  ${nationalForests.dispersedCampingAllowed && '<span class="dispersed-badge">Dispersed Camping Allowed</span>'}
 			  <p class="forestDistance">${nationalForests.distance} miles away</p>
 			  <p class="forestDescription">${nationalForests.description}</p>
 			  <a class="forestLink" href="${nationalForests.link}" target="_blank">View Official USFS Page</a>
